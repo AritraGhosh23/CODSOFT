834e1b96a43adebd7c8c83f01ee4a251ec1e8741
AritraGhosh23 <aritra02.akg@gmail.com>
2023-09-09 23:12:36 +0530

    Task 1

---
+++ guessingGame.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    cout << "\n\t\t\tWelcome to Number Guessing Game" << endl;
+    cout << endl;
+    int guessnum, realnum;
+    srand(time(0));             // seeding random number generator
+    realnum = rand() % 100 + 1; // generating a random no between 1 and 100
+    int count = 0;              // initialising count variable as 0
+    do
+    {
+        cout << "Enter your guess: " << endl; // taking input from user
+        cin >> guessnum;
+        count++;
+        if (guessnum < realnum)
+            cout << "Your guess is too low. Try again." << endl;
+        else if (guessnum > realnum)
+            cout << "Your guess is too high. Try again." << endl;
+        else
+            cout << "Congratulations! You guessed the right answer in " << count << " tries" << endl;
+
+    } while (guessnum != realnum); // loop stops once the user guesses the number correctly
+    return 0;
+}
